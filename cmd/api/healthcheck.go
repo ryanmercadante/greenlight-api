8d8b69a4da4a3ac3b56fbdbf8c308fbd9895e24a
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-04-28 16:17:50 -0400

    update healthcheckHandler to use json.Marshal method

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,20 +1,37 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 // Declare a handler which writes a plain-text response which information about
 // the application status, operating environment, and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	// Create a fixed-format JSON response from a string.
-	js := `{"status": "available", "environment": %q, "version": %q}`
-	js = fmt.Sprintf(js, app.config.env, version)
+	// Create a map which holds the information that we want to send in the response.
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	// Pass the map to the json.Marshal() function. This returns a []byte slice
+	// containing the encoded JSON. If there was an error, we log it and send the client
+	// a generic error message.
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+		return
+	}
+
+	// Append a newline to the JSON. This is just a small nicety to make it easier
+	// to view in terminal applications.
+	js = append(js, '\n')
 
 	// Set the "Content-Type: application/json" header on the response.
 	w.Header().Set("Content-Type", "application/json")
 
-	// Write the JSON as the HTTP response body.
-	w.Write([]byte(js))
+	// Use w.Write() to send the []byte slice containing the JSON as the response body.
+	w.Write(js)
 }
