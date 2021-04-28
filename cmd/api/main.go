9ff4d66703731a9dc53f3978fc84402fe734ccef
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-04-28 13:17:40 -0400

    setup main.go and add healthcheckHandler

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,68 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+// Application version number. Later on this will be generated at build time.
+const version = "1.0.0"
+
+// Define a config struct to hold all the configuration settings for the app.
+type config struct {
+	port int
+	env  string
+}
+
+// Define an application struct to hold the dependencies for HTTP handlers,
+// helpers, and middleware.
+type application struct {
+	config config
+	logger *log.Logger
+}
 
 func main() {
-	log.Println("Hello, World!")
+	var cfg config
+
+	// Read the value of the port and env command-line flags into the config struct.
+	// Default to using the port number 4000 and the environment "development" if no
+	// corresponding flags are provided.
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+	flag.Parse()
+
+	// Initialize a new logger which writes messages to the standard output stream,
+	// prefixed with the current date and time.
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	// Declare an instance of the application struct, containing the config struct and
+	// the logger.
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	// Declare a new servemux and add a /v1/healthcheck route which dispatches requests
+	// to the healthcheckHandler method
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+
+	// Declare a HTTP server with some sensible timeout settings, which listens on the
+	// port provided in the config struct and uses the servemux we created as the handler.
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	// Start the HTTP server.
+	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
 }
