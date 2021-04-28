612bc4e2563196c4cbadce9ca88812a5fd226f28
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-04-28 17:44:15 -0400

    update writeJSON helper to use json.MarshalIndent, create envelope for returning json to client

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -7,14 +7,16 @@ import (
 // Declare a handler which writes a plain-text response which information about
 // the application status, operating environment, and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	// Create a map which holds the information that we want to send in the response.
-	data := map[string]string{
-		"status":      "available",
-		"environment": app.config.env,
-		"version":     version,
+	// Declare an envelope map containing the data for the response.
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
 	}
 
-	err := app.writeJSON(w, http.StatusOK, data, nil)
+	err := app.writeJSON(w, http.StatusOK, env, nil)
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -33,7 +33,7 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 	}
 
 	// Encode the struct to JSON and send it as the HTTP response
-	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
