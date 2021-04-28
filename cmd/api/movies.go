de3d287643f91b462e80aa5ef92d5cae7108abbb
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-04-28 17:28:05 -0400

    add data package and Movie struct with struct tags for json output

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,6 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/ryanmercadante/greenlight-api/internal/data"
 )
 
 // Add a createMovieHandler for the "POST /v1/movies" endpoint.
@@ -18,6 +21,21 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	// Otherwise, interpolate the movie ID in a placeholder response
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	// Create a new instance of the Movie struct, containing the ID we extracted from
+	// the URL and some dummy data.
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	// Encode the struct to JSON and send it as the HTTP response
+	err = app.writeJSON(w, http.StatusOK, movie, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
