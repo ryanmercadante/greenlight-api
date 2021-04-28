42c6c9c23e1e1b635fa108d896305c8f5825cffa
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-04-28 15:58:09 -0400

    create helper function to get id param from url

---
+++ cmd/api/helpers.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+// Retrieve the "id" URL parameter from the current request context, then convert
+// it to an integer and return it. If the operation isn't successful, return 0
+// and an error.
+func (app *application) readIDParam(r *http.Request) (int64, error) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	if err != nil || id < 1 {
+		return 0, errors.New("invalid id parameter")
+	}
+
+	return id, nil
+}
--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,9 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 // Add a createMovieHandler for the "POST /v1/movies" endpoint.
@@ -15,17 +12,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 
 // Add a showMovieHandler for the "GET /v1/movies/:id" enpoint.
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
-	// When httprouter is parsing a request, any interpolated URL parameters will be
-	// stored in the request context. We can use ParamsFromContext() function to
-	// retrieve a slice containing these parameter names and values.
-	params := httprouter.ParamsFromContext(r.Context())
-
-	// We can use the ByName() method to get the value of the "id" parameter from
-	// the slice. In our project all movies will have a unique positive integer ID, but
-	// the value returned by ByName() is always a string. So we try to convert it to a
-	// base 10 integer (with a bit size of 64). If the parameter couldn't be converted,
-	// function to return a 404 Not Found response.
-	id, err := strconv.ParseInt(params.ByName("id"), 10, 64)
+	id, err := app.readIDParam(r)
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
 		return
