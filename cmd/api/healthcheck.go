da00d96499a214ed313e4791ebc6dc1ab0f8c4fe
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-04-28 17:06:50 -0400

    add writeJSON helper function and use in healthcheckHandler

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
 
@@ -15,23 +14,9 @@ func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Reques
 		"version":     version,
 	}
 
-	// Pass the map to the json.Marshal() function. This returns a []byte slice
-	// containing the encoded JSON. If there was an error, we log it and send the client
-	// a generic error message.
-	js, err := json.Marshal(data)
+	err := app.writeJSON(w, http.StatusOK, data, nil)
 	if err != nil {
 		app.logger.Println(err)
 		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
-		return
 	}
-
-	// Append a newline to the JSON. This is just a small nicety to make it easier
-	// to view in terminal applications.
-	js = append(js, '\n')
-
-	// Set the "Content-Type: application/json" header on the response.
-	w.Header().Set("Content-Type", "application/json")
-
-	// Use w.Write() to send the []byte slice containing the JSON as the response body.
-	w.Write(js)
 }
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -20,4 +21,35 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	}
 
 	return id, nil
+}
+
+// Define a writeJSON() helper for sending responses. This takes the destination
+// http.ResponseWriter, the HTTP status code to send, the data to encode to JSON,
+// and a header map containing any additional HTTP headers we want to include in
+// the response.
+func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
+	// Encode the data to JSON, returning the error if there was one.
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// Append a newline to make it easier to view in terminal applications
+	js = append(js, '\n')
+
+	// Add any headers that we want to include. We loop through the header map and
+	// add each header to the http.ResponseWriter header map. Note that it's OK if
+	// the provided header map is nil. Go doesn't throw an error if you try to range
+	// over (or generally, read from) a nil map.
+	for key, value := range headers {
+		w.Header()[key] = value
+	}
+
+	// Add the "Content-Type: application/json" header, then write the status code
+	// and JSON response.
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil
 }
