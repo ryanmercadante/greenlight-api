df3b4ab999cfb75d8a68f5a7f535d0c363fa1d47
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-05-07 17:15:27 -0400

    create Filters struct for reuse

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -220,11 +220,9 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 	// To keep things consistent with our other handlers, we'll define an input struct
 	// to hold the expected values from the request query string.
 	var input struct {
-		Title    string
-		Genres   []string
-		Page     int
-		PageSize int
-		Sort     string
+		Title  string
+		Genres []string
+		data.Filters
 	}
 
 	// Initialize a new Validator instance.
@@ -239,15 +237,13 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 	input.Title = app.readString(qs, "title", "")
 	input.Genres = app.readCSV(qs, "genres", []string{})
 
-	// Get the page and page_size query string values as integers. Notice that we set
-	// the default page value to 1 and default page_size to 20, and that we pass the
-	// validator instance as the final argument here.
-	input.Page = app.readInt(qs, "page", 1, v)
-	input.PageSize = app.readInt(qs, "page_size", 20, v)
+	// Read the page and page_size query string values into the embedded struct.
+	input.Filters.Page = app.readInt(qs, "page", 1, v)
+	input.Filters.PageSize = app.readInt(qs, "page_size", 20, v)
 
-	// Extract the sort query string value, falling back to "id" if it is not provided,
-	// by the client (which will imply an ascending sort on movie ID).
-	input.Sort = app.readString(qs, "sort", "id")
+	// Extract the sort query string value into the embedded struct, falling back to "id"
+	// if it is not provided, by the client (which will imply an ascending sort on movie ID).
+	input.Filters.Sort = app.readString(qs, "sort", "id")
 
 	// Check the Validator instance for any errors and use the failedValidationResponse()
 	// helper to send the client a response if necessary.
