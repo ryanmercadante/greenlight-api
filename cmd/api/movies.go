5fc489c110ccac1f061f89e6363a71640298b613
Ryan Mercadante <ryan.a.mercadante@gmail.com>
2021-05-07 17:24:19 -0400

    add filter validation check

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -244,10 +244,12 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 	// Extract the sort query string value into the embedded struct, falling back to "id"
 	// if it is not provided, by the client (which will imply an ascending sort on movie ID).
 	input.Filters.Sort = app.readString(qs, "sort", "id")
+	// Add the supported sort values for this endpoint to the sort safelist.
+	input.Filters.SortSafelist = []string{"id", "title", "year", "runtime", "-id", "-title", "-year", "-runtime"}
 
-	// Check the Validator instance for any errors and use the failedValidationResponse()
-	// helper to send the client a response if necessary.
-	if !v.Valid() {
+	// Execute the validation checks on the Filters struct and send a response
+	// containing the errors if necessary.
+	if data.ValidateFilters(v, input.Filters); !v.Valid() {
 		app.failedValidationResponse(w, r, v.Errors)
 		return
 	}
